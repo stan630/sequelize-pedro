b7b9e0f91f0e15d98390d84a67caed9b6bff3259
stan630 <cslafond@gmail.com>
2026-03-21 16:45:35 -0400

    Table data appear on client URL.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,33 @@
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  useEffect(()=>{
+  const [studentData,setStudentData] = useState([])
+  
+   useEffect(()=>{
    axios.get("http://localhost:8088/student").then((response)=>{
-      console.log(response)
+      setStudentData(response.data)
    })
   }, [])
 
   return (
-    <>
-      
-    </>
-  )
+    <div className='App'> 
+      {studentData.map((student,key) => {
+         return (
+            <div className='studentInfo'>
+               <div>{student.fname}</div>
+               <div>{student.lname}</div>
+               <div>{student.email}</div>
+               <div>{student.major}</div>
+               <div>{student.dorm}</div>
+               
+            </div>
+         )
+      })}
+      </div>
+    
+   )
 }
 
 export default App
